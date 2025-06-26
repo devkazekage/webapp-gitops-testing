936009c0bb18a31338ec751243a1683bac1fad86
cross-web <avi070440@gmail.com>
2025-06-26 16:59:54 +0530

    Update get-argocd-password.sh

--- terraform/get-argocd-password.sh
+++ terraform/get-argocd-password.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo "{\"password\": \"${PASSWORD}\"}"
-
-
-chmod +x terraform/get-argocd-password.sh
